b36fa60d97eb882686a905a328167f7cdffb5ef4
Oleg <olegashepelev@gmail.com>
2023-02-02 21:37:13 +0600

    Complete second task

---
+++ 02/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+
+Console.Clear();
+int m = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+int n = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+int sumNumbers = GetSumNumbers(m, n);
+Console.WriteLine($"M = {m}; N = {n} -> {sumNumbers}");
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetSumNumbers(int m, int n)
+{
+    if (m == n) return n;
+    else return n + GetSumNumbers(m, n - 1);
+}
